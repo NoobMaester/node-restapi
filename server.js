a7a25f730aaae0439270da09864cec03febba0d6
NoobMaester <joshstilez@gmail.com>
2024-02-20 11:59:28 +0200

    CRUD/rest API for goals

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv/config')
 const port = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
 //MIDDLEWARES
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: false}))
 
 // app.use(express.json())
 // app.use(express.urlencoded({extended: false}))
